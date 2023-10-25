7ea0122ec2b1de85760b868a544a382d4c4bec4f
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2023-10-25 23:21:04 +0200

    NEW

---
+++ src/GosGat/Main.java
@@ -0,0 +1,21 @@
+package GosGat;
+class Gos {
+    public void lladrar(){
+        System.out.println("Guau, guau");
+    }
+}
+
+class Gat {
+    public void miolar(){
+        System.out.println("Miau, miau");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+            Gos gos = new Gos();
+            Gat gat = new Gat();
+
+            gos.lladrar();
+            gat.miolar();
+        }
+}
