f4db027f38c0508467e8dfd4dd900e396f951d88
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2023-10-25 21:45:39 +0200

    NEW

--- src/CartaFormal.java
+++ src/CartaFormal.java
@@ -1,2 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CartaFormal {
+    //Declarar variables
+    public String tractament;
+    public String nom;
+    public String cognom1;
+    public String cognom2;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+
+        //Demanar
+        String tractament = scanner.nextLine();
+        String nom = scanner.nextLine();
+        String cognom1 = scanner.nextLine();
+        String cognom2 = scanner.nextLine();
+
+        //Imprimir
+        System.out.println(tractament + " " + cognom1 + " " + cognom2 + "," + " " + nom + "\n" );
+        System.out.println("El principal objectiu de la present carta...");
+    }
 }
