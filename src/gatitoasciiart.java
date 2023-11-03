19233a95486a2b757d16f9821d504d20e6919311
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2023-11-03 21:04:40 +0100

    NEW

---
+++ src/gatitoasciiart.java
@@ -0,0 +1,7 @@
+public class gatitoasciiart {
+    public static void main(String[] args) {
+        System.out.println("(\\___/)\n" +
+                "(=)\n" +
+                "(_(\")(\")");
+    }
+}
