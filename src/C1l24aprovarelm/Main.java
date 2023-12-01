6aa69a6f82b318384843aa1a9122eff0d13395ff
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2023-12-01 17:27:35 +0100

    Hola

--- src/C1l24aprovarelm/Main.java
+++ src/C1l24aprovarelm/Main.java
@@ -4,10 +4,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 class Modul {
-    public boolean aprovat(int numeroPrUf1, int numeroPrLlUf1, int notaExamUf1, int numeroPrUf2, int numeroPrLlUf2, int notaExamUf2, int numeroPrUf3, int numeroPrLlUf3, int notaExamUf3){
-        boolean aprovatuf1 = (numeroPrLlUf1 >= (75 * numeroPrUf1 /100) && notaExamUf1 >= 4) || ((numeroPrLlUf1 >= (50 * numeroPrUf1 /100) && notaExamUf1 >= 5) || notaExamUf1 > 7);
-        boolean aprovatuf2 = (numeroPrLlUf2 >= (75 * numeroPrUf2 / 100) && notaExamUf2 >= 4) || (numeroPrLlUf2 == numeroPrUf2) || (numeroPrLlUf2 >= (50 * numeroPrUf2 / 100) && notaExamUf2 >= 5);
-        boolean aprovatuf3 = (numeroPrLlUf3 == numeroPrUf3) && (notaExamUf3 >= 5);
+    public boolean modulAprovat(int PrUf1, int PrLlUf1, int nExUf1, int PrUf2, int PrLlUf2, int nExUf2, int PrUf3, int PrLlUf3, int nExUf3){
+        boolean aprovatuf1 = (PrLlUf1 >= (75 * PrUf1 /100) && nExUf1 >= 4) || ((PrLlUf1 >= (50 * PrUf1 /100) && nExUf1 >= 5) || nExUf1 > 7);
+        boolean aprovatuf2 = (PrLlUf2 >= (75 * PrUf2 / 100) && nExUf2 >= 4) || (PrLlUf2 == PrUf2) || (PrLlUf2 >= (50 * PrUf2 / 100) && nExUf2 >= 5);
+        boolean aprovatuf3 = (PrLlUf3 == PrUf3) && (nExUf3 >= 5);
         return aprovatuf1 && aprovatuf2 && aprovatuf3;
     }
 }
@@ -18,6 +18,6 @@ public class Main {
 
         Modul m1 = new Modul();
 
-        System.out.println(m1.aprovat(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        System.out.println(m1.modulAprovat(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
     }
 }
