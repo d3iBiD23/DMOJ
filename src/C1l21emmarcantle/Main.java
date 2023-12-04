54ed6e4ac64a68f9e45da31479d419628490f138
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2023-12-04 17:36:59 +0100

    Hola

--- src/C1l21emmarcantle/Main.java
+++ src/C1l21emmarcantle/Main.java
@@ -11,15 +11,31 @@ public class Main {
         //FOTO
         int xf1 = sc.nextInt();
         int yf1 = sc.nextInt();
+
         int xf2 = sc.nextInt();
         int yf2 = sc.nextInt();
 
         //MARCO
         int xm1 = sc.nextInt();
         int ym1 = sc.nextInt();
+
         int xm2 = sc.nextInt();
         int ym2 = sc.nextInt();
 
+        float alturaM = Math.abs(ym2 - ym1);
+        float baseM = Math.abs(xm2 - xm1);
 
+        float alturaF = Math.abs(yf2 - yf1);
+        float baseF = Math.abs(xf2 - xf1);
+
+        float proporcioF = (baseF / alturaF);
+        float proporcioM = (baseM / alturaM);
+
+        boolean cap = (alturaM >= alturaF && baseM >= baseF);
+        boolean proporcioIgual = (proporcioF == proporcioM);
+
+        boolean adequat = cap && proporcioIgual;
+
+        System.out.println(adequat);
     }
 }
