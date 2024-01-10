fafcf14a6a9465936d8fbbc11455f105db8a7ecc
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-01-10 21:44:04 +0100

    NEW

--- src/C1l24aprovarelm/Main.java
+++ src/C1l24aprovarelm/Main.java
@@ -17,9 +17,6 @@ abstract class UF{
     public boolean aprovar(){
         return true;
     }
-
-    public boolean aprovar(int actitud){
-    }
 }
 
 class UF1 extends UF{
@@ -29,7 +26,7 @@ class UF1 extends UF{
     }
     @Override
     public boolean aprovar(){
-
+        return (pEntregades == (75 * pTotals / 100) && (nExamen >= 4)) || (pEntregades == (50 * pTotals / 100) && nExamen >= 5) || (nExamen > 7);
     }
 }
 class UF2 extends UF{
@@ -39,7 +36,7 @@ class UF2 extends UF{
     }
     @Override
     public boolean aprovar(){
-
+        return (pEntregades == (75 * pTotals / 100) && nExamen >= 4) || (pEntregades == pTotals) || (pEntregades >= (50 * pTotals / 100) && nExamen >= 5);
     }
 }
 class UF3 extends UF{
@@ -49,14 +46,38 @@ class UF3 extends UF{
     }
     @Override
     public boolean aprovar(){
-
+        return (pEntregades == pTotals) && (nExamen >= 5);
     }
 }
+class Modul{
+    private final UF1 uf1;
+    private final UF2 uf2;
+    private final UF3 uf3;
+
+    public Modul(UF1 uf1, UF2 uf2, UF3 uf3) {
+        this.uf1 = uf1;
+        this.uf2 = uf2;
+        this.uf3 = uf3;
+    }
+
+    public boolean isModulAprovat(){
+        return uf1.aprovar() && uf2.aprovar() && uf3.aprovar();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        UF1 uf1 = new UF1(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        UF2 uf2 = new UF2(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        UF3 uf3 = new UF3(sc.nextInt(), sc.nextInt(), sc.nextInt());
 
+        Modul modul1 = new Modul(uf1, uf2, uf3);
+
+        boolean isModulAprovat = modul1.isModulAprovat();
+
+        System.out.println(isModulAprovat);
     }
 }
