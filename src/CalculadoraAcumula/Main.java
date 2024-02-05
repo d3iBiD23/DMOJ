62cdf9febe7d43770f1bfa18b99ce0c83dcce9c4
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-05 17:42:47 +0100

    Hola

--- src/CalculadoraAcumula/Main.java
+++ src/CalculadoraAcumula/Main.java
@@ -3,40 +3,60 @@ package CalculadoraAcumula;
 import java.util.Locale;
 import java.util.Scanner;
 
+class Calculadora{
+    private float acc = 0.0f;
+
+    public float getAcc() {
+        return this.acc;
+    }
+
+    public float suma(float numero){
+        return this.acc += numero;
+    }
+    public float resta(float numero){
+        return this.acc -= numero;
+    }
+    public float multi(float numero){
+        return this.acc *= numero;
+    }
+    public float divisio(float numero){
+        return this.acc /= numero;
+    }
+    public Calculadora() {
+    }
+}
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        float acc = 0;
+        Calculadora calc = new Calculadora();
 
-        while (sc.hasNext()) {
+        String operador = sc.next();
 
-            String operador = sc.next();
-
-            if (operador.equals("END")) {
-                break;
-            }
-
-            float numero = sc.nextFloat();
-
-            acc = operacion(operador, numero, acc);
-
-            System.out.println(acc);
+        if (operador.equals("END")){
+            System.out.println(calc.getAcc());
         }
-    }
-    public static float operacion(String operador, float numero, float acc) {
-        switch (operador) {
-            case "+":
-                return acc + numero;
-            case "-":
-                return acc - numero;
-            case "*":
-                return acc * numero;
-            case "/":
-                return acc / numero;
-            default:
-                return acc;
+
+        while (!operador.equals("END")){
+            float operand = sc.nextFloat();
+
+            switch (operador){
+                case "+":
+                    calc.suma(operand);
+                    break;
+                case "-":
+                    calc.resta(operand);
+                    break;
+                case "*":
+                    calc.multi(operand);
+                    break;
+                case "/":
+                    calc.divisio(operand);
+                    break;
+            }
+            System.out.println(calc.getAcc());
+            operador = sc.next();
         }
     }
 }
