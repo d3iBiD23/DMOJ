5567b319948164b4853197856e531fa9d3397a38
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-07 14:49:31 +0100

    New DMOJ problems solved.

--- src/C3l31robotsimula/Main.java
+++ src/C3l31robotsimula/Main.java
@@ -3,10 +3,52 @@ package C3l31robotsimula;
 import java.util.Locale;
 import java.util.Scanner;
 
+class Robot{
+    private int coordX;
+    private int coordY;
+    private String orientacio;
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    public String getOrientacio() {
+        return orientacio;
+    }
+
+    public Robot(int coordX, int coordY, String orientacio) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.orientacio = orientacio;
+    }
+    public void girDreta(){
+
+    }
+    public void girEsq(){
+
+    }
+    public void endavant(){
+
+    }
+}
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
+
+        int x = sc.nextInt();
+
+        int y = sc.nextInt();
+
+        String orientacio = sc.next();
+
+        String ordre = sc.nextLine();
+
+        Robot r = new Robot(x, y, orientacio);
 
 
     }
