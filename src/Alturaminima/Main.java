3e55e234ba7e8cb24570769e6152a627dd24cd20
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-07 19:48:24 +0100

    Hola

---
+++ src/Alturaminima/Main.java
@@ -0,0 +1,34 @@
+package Alturaminima;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int qtyNens = sc.nextInt();
+
+        float[] arrayNens = new float[qtyNens];
+
+
+        for (int i = 0; i < qtyNens; i++){
+            float altura = sc.nextFloat();
+
+            arrayNens[i] = altura;
+        }
+
+        float alturaMin = sc.nextFloat();
+
+        for (int i = 0; i < qtyNens; i++){
+            if (arrayNens[i] < alturaMin){
+                System.out.println("NO");
+            }else {
+                System.out.println("SI");
+            }
+        }
+
+        sc.close();
+    }
+}
