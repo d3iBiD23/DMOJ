993dcbf88f01fdbaa515d691e89c64f99f552050
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-07 22:11:04 +0100

    New DMOJ problems solved.

--- src/BuscarLletres/Main.java
+++ src/BuscarLletres/Main.java
@@ -8,5 +8,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        char letra = sc.nextLine().charAt(0);
+
+        String frase = sc.nextLine();
+
+        sc.close();
+
+        int resultado = contarOcurrencias(letra, frase);
+
+        System.out.println(resultado);
+    }
+
+    private static int contarOcurrencias(char letra, String frase) {
+        char[] array = frase.toCharArray();
+
+        int contador = 0;
+
+        for (char c : array) {
+            if (c == letra) {
+                contador++;
+            }
+        }
+
+        return contador;
     }
 }
---
+++ src/IgualesALultimo/Main.java
@@ -0,0 +1,15 @@
+package IgualesALultimo;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int qtyNum = sc.nextInt();
+
+
+    }
+}
