20e315845a8c0db738cac896120dbe50d002406f
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-08 15:41:34 +0100

    New DMOJ problems solved.

--- src/BuscarLletres/Main.java
+++ src/BuscarLletres/Main.java
@@ -8,24 +8,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        char letra = sc.nextLine().charAt(0);
+        char lletra = sc.nextLine().charAt(0);
 
         String frase = sc.nextLine();
 
-        sc.close();
+        int resultat = contarLletres(lletra, frase);
 
-        int resultado = contarOcurrencias(letra, frase);
+        System.out.println(resultat);
 
-        System.out.println(resultado);
     }
 
-    private static int contarOcurrencias(char letra, String frase) {
+    private static int contarLletres(char lletra, String frase){
         char[] array = frase.toCharArray();
 
         int contador = 0;
 
-        for (char c : array) {
-            if (c == letra) {
+        for (char c : array){
+            if (lletra == c) {
                 contador++;
             }
         }
--- src/IgualesALultimo/Main.java
+++ src/IgualesALultimo/Main.java
@@ -10,6 +10,25 @@ public class Main {
 
         int qtyNum = sc.nextInt();
 
+        int[] arrayNums = new int[qtyNum];
 
+        for (int i = 0; i < qtyNum; i++){
+
+            int numero = sc.nextInt();
+
+            arrayNums[i] = numero;
+        }
+
+        int ultimElement = arrayNums[arrayNums.length - 1];
+
+        int contador = 0;
+
+        for (int i = 0; i < arrayNums.length - 1; i++) {
+            if (arrayNums[i] == ultimElement) {
+                contador++;
+            }
+        }
+
+        System.out.println(contador);
     }
 }
