da256dc4493dc67a51edf4df2e260776245bdb02
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-02-20 17:18:49 +0100

    Hola

--- src/Examen/Main.java
+++ src/Examen/Main.java
@@ -3,42 +3,36 @@ package Examen;
 import java.util.Locale;
 import java.util.Scanner;
 
-class Alumne{
-    private float notaEx1;
-    private float notaEx2;
-    private float notaEx3;
-
-    public Alumne(float notaEx1, float notaEx2, float notaEx3) {
-        this.notaEx1 = notaEx1;
-        this.notaEx2 = notaEx2;
-        this.notaEx3 = notaEx3;
-    }
-
-    public String missatgeNota(){
-        if (this.notaEx1 < 5 && this.notaEx2 < 5 && this.notaEx3 < 5){
-            return "No s'ha aprovat cap examen.";
-        }else if (this.notaEx1 >= 5 && this.notaEx2 < 5 && this.notaEx3 < 5 || this.notaEx1 < 5 && this.notaEx2 >= 5 && this.notaEx3 < 5 || this.notaEx1 < 5 && this.notaEx2 < 5 && this.notaEx3 >= 5){
-            return "S'ha aprovat un examen.";
-        } else if (this.notaEx1 >= 5 && this.notaEx2 >= 5 && this.notaEx3 < 5 || this.notaEx1 < 5 && this.notaEx2 >= 5 && this.notaEx3 >= 5 || this.notaEx1 >= 5 && this.notaEx2 < 5 && this.notaEx3 >= 5) {
-            return "S'han aprovat dos exàmens.";
-        }else{
-            return "S'han aprovat tots els exàmens.";
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        int nAlumnes = sc.nextInt();
+        String[] fila1 = sc.nextLine().split(" ");
 
+        String[] fila2 = sc.nextLine().split(" ");
 
-        for (int i = 0; i < nAlumnes; i++){
-            Alumne a = new Alumne(sc.nextFloat(), sc.nextFloat(), sc.nextFloat());
+        int posicio = sc.nextInt();
 
-            System.out.println(a.missatgeNota());
+        compararLinies(fila1, fila2, posicio);
+
+        sc.close();
+
+    }
+    private static void compararLinies(String[] fila1, String[] fila2, int posicio){
+
+        for (int i = 0; i < fila1.length; i++){
+            if (fila1[posicio].charAt(0) == fila2[posicio].charAt(0)){
+                System.out.println("La paraula a la posició " + posicio + " comença per la mateixa lletra a les dues línies.");
+            }else {
+                System.out.println("La paraula a la posició " + posicio + " comença per lletres diferents a les dues línies.");
+            }
+            if (fila1[i].equals(fila2[i])){
+                System.out.println("Totes les paraules són les mateixes.");
+            }else {
+                System.out.println("Les paraules no són les mateixes.");
+            }
+            break;
         }
     }
 }
