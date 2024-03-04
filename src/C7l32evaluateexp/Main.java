9e0d371883476583b5889f46ffa8b62c3966fc1d
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-03-04 11:15:16 +0100

    New DMOJ problems solved.

--- src/C7l32evaluateexp/Main.java
+++ src/C7l32evaluateexp/Main.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
 
-        String expresion = scanner.nextLine();
+        String expressio = sc.nextLine();
 
-        scanner.close();
+        // Dividir l'String en els operadors "+" i obtenir els sumands
+        String[] numeros1 = expressio.split("\\+");
+
+        int resultatP = 0;
+
+        for (String num : numeros1){
+            // Dividir cada sumand en els operadors "*" i multiplicar els valors
+            String[] factors = num.split("\\*");
+            int resultat = 1;
+
+            for (String factor : factors){
+                // Multiplicar cada factor convertit a Int
+                resultat *= Integer.parseInt(factor.trim());
+            }
+
+            // Sumar el resultat parcial al resultat total
+            resultatP += resultat;
+        }
+        System.out.println(resultatP);
     }
 }
