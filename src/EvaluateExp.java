cb3175c4be2dadfd79d783781b941f889efad0a2
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-03-06 19:26:04 +0100

    Hola

--- src/EvaluateExp.java
+++ src/EvaluateExp.java
@@ -1,9 +1,7 @@
-package C7l32evaluateexp;
-
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class EvaluateExp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
